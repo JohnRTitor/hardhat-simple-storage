5d10395a414c91e5ac8dc04ea027eb11c32a320f
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2025-03-07 23:03:01 +0530

    First contract deploy.ts
    Signed-off-by: John Titor <50095635+JohnRTitor@users.noreply.github.com>

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,9 +1,20 @@
 // imports
-//
-// async main
-async function main(): Promise<void> {}
-// main call
+import { ethers } from "hardhat";
+import { SimpleStorage__factory } from "../typechain-types";
 
+// async main
+async function main(): Promise<void> {
+  const SimpleStorageFactory: SimpleStorage__factory =
+    await ethers.getContractFactory("SimpleStorage");
+
+  console.log("Deploying contract...");
+  const simpleStorage = await SimpleStorageFactory.deploy();
+  await simpleStorage.waitForDeployment();
+
+  console.log(`Deployed contract to ${await simpleStorage.getAddress()}`);
+}
+
+// main call
 main()
   .then(() => {
     console.log("Deployed successfully!");
